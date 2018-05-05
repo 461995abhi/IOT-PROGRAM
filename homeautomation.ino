30b2d758a81c1054b79713b25ad5e4d85ecb6dc9
ABHILASHA  KUMARI <35931783+461995abhi@users.noreply.github.com>
2018-05-05 20:42:03 +0530

    Add files via upload

---
+++ homeautomation.ino
@@ -0,0 +1,44 @@
+#include <ESP8266WiFi.h>
+
+#include<WiFiUdp.h>
+WiFiUDP    udp;
+char*  ssid= "AndroidAP";
+char*  password ="qwertyui";
+
+char  msg[20];
+int led=5;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+WiFi.begin(ssid, password);
+if( WiFi.status()!=WL_CONNECTED)
+{
+  Serial.println("wait, wifi is not connected ");
+  delay(1000);
+  }
+
+pinMode(5,OUTPUT);
+
+udp.begin(50000);
+udp.read(msg,20);
+
+
+}
+
+void loop() {
+  Serial.println(WiFi.localIP());
+Serial.println("Wifi  is connected , wait..");
+delay(1000);
+String  Logic =String(msg);
+Serial.println(msg);
+if(Logic.equals("ON"))
+{
+  digitalWrite(led,HIGH);
+  delay(1000);
+}
+else 
+{
+  digitalWrite(led,LOW);
+  delay(1000);
+}
+}
